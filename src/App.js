268c860ea875937ba4e64d17971b92c9c9688cdb
Sahil1602 <sb160295@gmail.com>
2020-05-17 12:14:34 +0530

    main change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import MyChart from './chart';
+import MyChart from './Chart';
 // import { PieChart } from 'react-minimal-pie-chart';
 class App extends React.Component {
 
@@ -8,23 +8,7 @@ class App extends React.Component {
 
   render(){
     return (
-      <div className="App">
-        {/* <from className='email-form'>
-          <label>Email id</label>
-          <input type='email' name='email' placeholder='example@example.com' />
-          <label>Subject</label>
-          <input type='text' name='subject' />
-          <textarea id='message' name='message' cols='50' rows='20' placeholder='type your message here!'></textarea>
-          <input type='submit' name='submit' value='send' />
-        </from> */}
-        {/* <PieChart className = 'pie-chart'  data = {[
-          { title: 'One', value: 10, color: '#E38627' },
-          { title: 'Two', value: 15, color: '#C13C37' },
-          { title: 'Three', value: 20, color: '#6A2135' },
-          { title: 'four', value: 3, color: '#6A993F'}
-        ]}/>; */}
-        <MyChart />
-      </div>
+      <MyChart />
     )
   }
   
--- src/Chart.js
+++ src/Chart.js
@@ -3,67 +3,33 @@ import {Bar, Line, Doughnut} from 'react-chartjs-2';
 import './Chart.css'
 
 class MyChart extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            chartData: {
-                radius: 50,
-                labels: ['Mumbai','Delhi','Bangalore','Hyderabad','Kolkata'],
-                datasets: [{
-                    label: 'population',
-                    data: [ 12442373, 11007835, 8436675, 6809970, 4486679 ],
-                    backgroundColor: ['green', 'coral', 'cyan', 'brown', 'red'],
-                }]
-            }
+    state = {
+        chartData: {
+            radius: 50,
+            labels: ['Mumbai','Delhi','Bangalore','Hyderabad','Kolkata'],
+            datasets: [{
+                label: 'population',
+                data: [ 12442373, 11007835, 8436675, 6809970, 4486679 ],
+                backgroundColor: ['green', 'coral', 'cyan', 'brown', 'red'],
+            }]
         }
     }
+
+
+    sum = this.state.chartData.datasets[0].data.reduce((sum, num)=>{
+        return sum + num;
+    }, 0);
+
     render(){
         return(
             <div className= 'chart'>
-            {/* <Bar
-                data={this.state.chartData}
-                // height={350}
-                // width={50}
-                options={{
-                    title:{
-                        display: true,
-                        text: 'Largest cities in India',
-                        fontSize: 25
-                      },
-                      legend:{
-                        display: false,
-                        position: 'left',
-                        labels:{
-                          fontColor: 'red'
-                        }
-                      }
-                }}
-            />
-            <Line
-                data={this.state.chartData}
-                // height={350}
-                // width={50}
-                options={{
-                    title:{
-                        display: true,
-                        text: 'Largest cities in India',
-                        fontSize: 25
-                      },
-                      legend:{
-                        display: false,
-                        position: 'left',
-                        labels:{
-                          fontColor: 'red'
-                        }
-                      }
-                }}
-            /> */}
+            
             <div className='percentPopulation'>
-                <h4>Mumbai: {(12442373/(12442373+11007835+8436675+6809970+4486679)*100).toFixed(2)}%</h4>
-                <h4>Delhi: {(11007835/(12442373+11007835+8436675+6809970+4486679)*100).toFixed(2)}%</h4>
-                <h4>Bangalore: {(8436675/(12442373+11007835+8436675+6809970+4486679)*100).toFixed(2)}%</h4>
-                <h4>Hyderabad: {(6809970/(12442373+11007835+8436675+6809970+4486679)*100).toFixed(2)}%</h4>
-                <h4>Kolkata: {(4486679/(12442373+11007835+8436675+6809970+4486679)*100).toFixed(2)}%</h4>
+                <h4>Mumbai: {((12442373/this.sum)*100).toFixed(2)}%</h4>
+                <h4>Delhi: {((11007835/this.sum)*100).toFixed(2)}%</h4>
+                <h4>Bangalore: {((8436675/this.sum)*100).toFixed(2)}%</h4>
+                <h4>Hyderabad: {((6809970/this.sum)*100).toFixed(2)}%</h4>
+                <h4>Kolkata: {((4486679/this.sum)*100).toFixed(2)}%</h4>
             </div>
             <Doughnut className='doughnutChart' 
                 data={this.state.chartData}
